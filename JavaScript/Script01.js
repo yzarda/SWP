3932635cc2a457bbf38a7f825985ad1c1ff6faa5
Arda Yildiz <ardayildiz.business@gmail.com>
2023-01-16 19:59:40 +0100

    COMMIT

---
+++ JavaScript/Script01.js
@@ -0,0 +1,11 @@
+let a = 17.4;
+let b = 3.9;
+
+let result = a / b;
+
+console.log("The Result of 17.4 divided by 3.9 is " + result);
+
+let firstname = ("Sefa");
+let lastname = ("Oeztuerk");
+
+console.log("Ich heiße " + firstname + " " + lastname);
