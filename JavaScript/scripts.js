eadb99e8acdb7c0ac130ec00f0f28b89f132b8f3
Arda Yildiz <ardayildiz.business@gmail.com>
2023-01-19 11:03:30 +0100

    JS

---
+++ JavaScript/scripts.js
@@ -0,0 +1,21 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+//Variablen
+
+let a = 7;
+let b = 8;
+let c = a + b;
+
+console.log("A hat den Wert " + a);
+
+//a = 10;
+
+console.log("A hat den Wert " + a);
+console.log("A + B = " + c);
+
+//Rechnungsarten
+// *, / , +, -
+
+c = b - a;
+console.log("b - a = " +c)
