a3ddfb45771d3cafc4e368a8667317ba6e620766
Arda Yildiz <ardayildiz.business@gmail.com>
2023-01-19 11:42:45 +0100

    JS3

---
+++ JavaScript/Variablen.js
@@ -0,0 +1,40 @@
+// Erstelle eine Variable banana mit dem Wert "Banane"
+// Erstelle eine Variable apple mit dem Wert "Apple"
+
+// Erstelle eine Variable bananaPricePerKilo mit dem Wert 2.14
+// Erstelle eine Variable applePricePerKilo mit dem Wert 3.43
+
+// Ein Apfel hat durchschnittlich ein Gewicht von 0.34 Kilo
+// Eine Banane hat durchschnittlich ein Gewicht von 0.22 Kilo
+
+// Gib folgendes aus: Anzahl Bananen/Äpfel pro Kilo, Preis pro Banane/Apfel
+// Preis von 8kg Äpfeln
+// Preis von 17kg Bananen
+// Preis von 1 Tonne Äpfel
+// Preis von 1 Tonne Bananen
+
+
+
+let fruit1 = "Banane";
+let fruit2 = "Apple";
+
+let bananaPriceKilo = 2.14;
+let applePricePerKilo = 3.43;
+
+let AppleWeightAverage = 2.14 + " Kilo";
+let BananaWeightAverage = 0.22 + " Kilo";
+
+console.log("Anzahl Bananen " + BananaWeightAverage + ", " + "anzahl Äpfel " + AppleWeightAverage);
+let resultApple = applePricePerKilo * 8;
+let resultBanana = bananaPriceKilo * 17;
+let resultAppleTon = applePricePerKilo * 1000;
+let resultBananaTon = bananaPriceKilo * 1000;
+console.log("Preis von 8kg Äpfel = " + resultApple);
+console.log("Preis von 17kg Bananen = " + resultBanana);
+console.log("Preis von 1000 Äpfel = " + resultAppleTon);
+console.log("Preis von 1000 Bananen = " + resultBananaTon);
+
+
+
+
+
---
+++ JavaScript/script3.js
@@ -0,0 +1,51 @@
+let a = 7;
+let b = 8;
+
+let ab = a + b;
+
+
+
+// +, -, *, /
+
+let c = 7.4;
+let d = 7.5;
+
+
+result = c + d;
+
+console.log(result);
+
+
+//Texte STRINGS
+
+let name1 = "Arda";
+console.log(name1);
+
+
+console.log("Das Ergebnis von  " + a + " und " + b + " ist " + ab);
+
+/*	1) Berechne das Ergebnis von 17.4 / 3.9. Gib folgenden Satz aus:
+
+	The result of 17.4 divided by 3.9 is 4.461538461538462
+
+	2) Lege eine Variable firstname und eine Varialbe lastname an. Weise den Variablen Deinen Namen zu. Gib folgendes aus:
+
+	"Ich heiße VORNAME NACHNAME"
+*/
+
+
+//1)
+
+a = 17.4;
+b = 3.9;
+
+let result2 = a / b;
+
+console.log("The result of " + a + " divided by " + b + " is " + result2);
+
+
+//2)
+
+let firstname = "Arda";
+let lastname = "Yildiz";
+console.log("Ich heisse " +firstname + " "+ lastname);
