2b0fdbb3b1c918c8b4e4abca348d8132bf3d5653
Arda Yildiz <ardayildiz.business@gmail.com>
2023-01-19 11:14:21 +0100

    JS2

---
+++ JavaScript/script2.js
@@ -0,0 +1,60 @@
+console.log("Hallo");
+
+console.log("Tolle Sache");
+
+
+// Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+
+
+
+// Berechne folgendes und gib das Eregebnis über console.log aus
+
+let result = a + b + c;
+console.log("Das Ergebnis ist " + result);
+
+
+// a + b + c
+
+let result1 = a + b + c;
+console.log("Das Ergebnis ist " + result1);
+
+// (a + b) / c
+
+let result2 = ((a+b) / c);
+console.log("Das Ergebnis ist " + result2);
+
+// c -a + b
+
+let result3 = c - a + b;
+console.log("Das Ergebnis ist" + result3)
+
+// a + b + c * 3
+
+let result4 = a + b + (c *3);
+console.log("Das Ergebnis ist" + result4);
+
+
+// (a + b + c)/4
+
+let result5 = (a + b + c) / 4;
+console.log("Das Ergebnis ist" + result5);
+
+
+// Ändere die Variablen auf folgende Werte
+// a = 3
+a= 3;
+
+// b = a + 3
+
+b = a + 3;
+
+// c = c -3
+
+c = c - 3;
+
+// gib die neuen Werte der Variablen aus
+
+console.log("Der neue Wert für A lautet" + a + ", der neue Wert für B lautet" + b + "und der Wert für C lautet" + c);
