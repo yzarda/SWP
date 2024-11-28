b189e3f2da6973c3fb22e3c61ec2e7d71a179ade
yzarda <ardayildiz.business@gmail.com>
2024-11-28 13:18:35 +0100

    Test File und Verbesserung Base

---
+++ Alghoritmen/DataGenerator/DataGenerator.java
@@ -0,0 +1,29 @@
+package at.riedmann.algorithms.dataGenerator;
+
+import java.util.Random;
+
+public class DataGenerator {
+    public static int[] generateDataArray(int size){
+        Random random = new Random();
+        int[] data = new int[size];
+        for (int i = 0; i < size; i++) {
+            data[i] = random.nextInt();
+        }
+        return data;
+    }
+
+    public static int[] generateDataArray(int size, int min, int max){
+        Random random = new Random();
+        int[] data = new int[size];
+        for (int i = 0; i < size; i++) {
+            data[i] = min + random.nextInt(max-min);
+        }
+        return data;
+    }
+
+    public static void printArray(int[] data){
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i]);
+        }
+    }
+}
---
+++ Alghoritmen/DataGenerator/DataGeneratorTest.java
@@ -0,0 +1,28 @@
+package at.riedmann.algorithms.dataGenerator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DataGeneratorTest {
+    @Test
+    void testGenerateDataArray() {
+        int[] data = DataGenerator.generateDataArray(10);
+        Assertions.assertEquals(data.length, 10);
+    }
+
+    @Test
+    void testGenerateDataArray2() {
+        int[] data = DataGenerator.generateDataArray(1000, 0, 10);
+        Assertions.assertEquals(data.length, 1000);
+        for (int i = 0; i < data.length; i++) {
+            Assertions.assertTrue(data[i] >= 0);
+            Assertions.assertTrue(data[i] <= 10);
+        }
+    }
+
+    @Test
+    void testPrintArray() {
+        int[] data = DataGenerator.generateDataArray(10, 0, 10);
+        DataGenerator.printArray(data);    
+    }
+}
